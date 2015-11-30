abf387f593232075567de61b4b58008391e0e970
Niels Lensink <niels.lenssink@kpn.com>
2015-11-30 17:59:15 +0100

    OPT version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='py-timeexecution',
-    version='0.0.3',
+    version='0.0.4',
     description="Python project",
     long_description=open('README.md').read(),
     author="",
