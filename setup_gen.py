ba0153c1e1a79a2d1a74b10416547e84a11b395c
Niels Lensink <niels.lenssink@kpn.com>
2015-12-01 17:03:08 +0100

    OPT new pkgversion implementation with write_setup_py

--- setup_gen.py
+++ setup_gen.py
@@ -1,21 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import os
 from pkgversion import list_requirements, pep440_version, write_setup_py
+from setuptools import find_packages
 
-template = """
-from setuptools import find_packages, setup
-
-setup(
+write_setup_py(
     name='py-timeexecution',
-    version='{version}',
+    version=pep440_version(),
     description="Python project",
     long_description=open('README.md').read(),
     author="",
     author_email='',
     url='https://stash.kpnnl.local/DE/py-timeexecution',
-    install_requires={install_requires},
-    packages=find_packages(exclude=['timeexecution.tests*']),
+    install_requires=list_requirements('requirements/requirements-base.txt'),
+    packages=find_packages(exclude=['tests*']),
     tests_require=['tox'],
     include_package_data=True,
     zip_safe=False,
@@ -29,11 +26,4 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Topic :: Internet :: WWW/HTTP',
     ]
-)
-"""
-
-write_setup_py(
-    template,
-    version=pep440_version(),
-    install_requires=list_requirements('requirements/requirements-base.txt')
 )
